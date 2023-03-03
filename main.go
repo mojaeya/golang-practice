e2c596b46f6a99b5383d80ad40b99aac273ee83e
mojaeya <mojaeya@gmail.com>
2023-03-03 13:50:31 +0900

    back 11

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"database/sql"
+	"golang-practice/api"
+	db "golang-practice/db/sqlc"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannnot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannnot start server:", err)
+	}
+}
